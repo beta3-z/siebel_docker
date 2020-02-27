96a8bb588f8546c47955c0bce4a9e967dcfb00e9
MaximEmelyanchenko <beta3-z@mail.ru>
2020-02-27 23:18:03 +0300

    Add 17IP enterprise server and application interface installation

---
+++ 4_enterprise_server/config/install.sh
@@ -0,0 +1,59 @@
+#! /bin/bash
+#yum install -y --skip-broken java-1.8.0-openjdk;
+#
+## X11
+#yum install -y \
+#	install -y epel-release
+#	groupinstall -y "Xfce"
+#
+## IP16
+#yum install -y --skip-broken \
+#	libnsl.i686 \
+#	libXtst.i686;
+#
+## IP19 ?
+#yum install -y --skip-broken \
+#	libgcc.i686 \
+#	libxcb.i686 \
+#	libX11.i686 \
+#	libXext.i686 \
+#	libXau.i686 \
+#	libcurl.i686;
+#
+#
+#rm -rf /var/cache/yum/x86_64;
+#rm -rf /var/cache/yum/i686;
+#yum clean all;
+#
+#groupadd -g  487 siebel;
+#useradd -m -g siebel -G siebel -u 9999  siebel;
+mkdir /siebel;
+
+
+chown -R siebel /siebel;
+chgrp -R siebel /siebel;
+
+chown -R siebel /Siebel_Install_Image;
+chgrp -R siebel /Siebel_Install_Image;
+#Xvfb :99 -screen 0 1280x1024x24 &
+#export DISPLAY=:99
+export JDK_PATH=$(alternatives --list | grep ^jre_1.8.0_openjdk | awk '{print $3}')
+#export JDK_PATH=/jdk/$(ls /jdk)
+export INSTALL_PATH=/Siebel_Install_Image/17.0.0.0/Linux/Server/Siebel_Enterprise_Server/Disk1/install
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/lib64"
+ln -s /lib64/libnsl.so.2 /lib64/libnsl.so.1
+
+su siebel -c "
+	${INSTALL_PATH}/runInstaller.sh \
+		-showProgress \
+		-silent \
+		-oneclick \
+		-responseFile /config/ses.rsp \
+		-invPtrLoc /config/oraInst.loc \
+		-waitforcompletion 
+";
+
+chmod -R 755 /siebel;
+
+rm -rf /Siebel_Install_Image /config
+
--- build_enterprise_server.bat
+++ build_enterprise_server.bat
@@ -1,10 +1,10 @@
 @echo off
 REM SET IMAGES_PATH=%cd:\=/%/Siebel_Install_Image
 REM SET CONFIG_PATH=%cd:\=/%/2_gateway/volume
-SET IMAGE_NAME=siebel_17_gtw
+SET IMAGE_NAME=siebel_17_ses
 
 docker image rm %IMAGE_NAME%
-docker build -f ./2_gateway/Dockerfile -t %IMAGE_NAME% .
+docker build -f ./4_enterprise_server/Dockerfile -t %IMAGE_NAME% .
 
 REM docker run --rm -v %IMAGES_PATH%:/Siebel_Install_Image -v CONFIG_PATH:/config -w / senpai /config/install_gateway.sh
 REM docker run --rm -v %IMAGES_PATH%:/Siebel_Install_Image -w /Siebel_Install_Image -it senpai bash
